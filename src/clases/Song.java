c2a2636c2171b9f9b3cee443a658d023d5786a35
elkingonzalezcuellar <elkin.gonzalez-c@mail.escuelaing.edu.co>
2022-02-14 20:53:16 -0500

    ClaseSong
    Se creo la clase song con sus atributos y metodos

---
+++ src/clases/Song.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+import java.util.Date;
+
+/**
+ *
+ * @author PC
+ */
+public class Song {
+
+    private String title, coverPage, description;
+    private int id;
+
+    private enum gender {
+        salsa, merengue, guaracha, vallenato, popular, reggaeton, rock, metal,
+        pop, bachata
+    };
+    private Date date;
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the coverPage
+     */
+    public String getCoverPage() {
+        return coverPage;
+    }
+
+    /**
+     * @param coverPage the coverPage to set
+     */
+    public void setCoverPage(String coverPage) {
+        this.coverPage = coverPage;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+}
