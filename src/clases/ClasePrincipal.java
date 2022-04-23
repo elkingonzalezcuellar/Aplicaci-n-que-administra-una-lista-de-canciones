a64e7014d3c0aac29dcfdfbfe7e43c923f88fb5c
JuanR <ramirezjuan9806@gmail.com>
2022-04-23 14:04:19 -0500

    Arreglos de errores en el flujo de trabajo y mejoramiento visual.

--- src/clases/ClasePrincipal.java
+++ src/clases/ClasePrincipal.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 
 public class ClasePrincipal {
     PlayList playList=new PlayList();
-    SongsLibrary principalLibrary = new SongsLibrary();
+    static SongsLibrary principalLibrary = new SongsLibrary();
+
 
     public static void main(String[] args) {
         ClasePrincipal claseP=new ClasePrincipal();
-
+        principalLibrary.storeObjects();
         claseP.mainMenu();
     }
     /**
@@ -45,16 +46,26 @@ public class ClasePrincipal {
                 } while (flag == 0);
 
                 if (selection == 1) {
-                    System.out.println("BIBLIOTECA PRINCIPAL");
-                    principalLibrary.storeObjects();
+                    System.out.println("/*\n" +
+
+                            "xxxxxxxxxxx   xxxxxxxxxx  xxxxxxxxxxx   xx      xxxxxxxxxx   xxxxxxx   xxxxxxxxxx xxxxxxxx    xxxxxxxx  xxxxxxxxx\n" +
+                            "xx        xx      xx      xx        xx  xx          xx     xxx     xxx     xx     x          xx         xx     xx\n" +
+                            "xx        xx      xx      xx          x xx          xx    x           xx   xx     x        x            xx     xx\n" +
+                            "xx       xx       xx      xx         xx xx          xx    x            x   xx     x        x            xx     xx\n" +
+                            "xxxxxxxxxxx       xx      xxxxxxxxxxxx  xx          xx    x            x   xx     xxxxxxxx x            xxxxxxxxx\n" +
+                            "xx        xx      xx      xx         xx xx          xx    xx           x   xx     x        x            xx     xx\n" +
+                            "xx        xx      xx      xx        xxx xx          xx      xx       xx    xx     x         xx          xx     xx\n" +
+                            "xxxxxxxxxxx   xxxxxxxxxx  xxxxxxxxxxxx  xxxxxxxx xxxxxxxxxx  xxxxxxxxx     xx     xxxxxxxxx  xxxxxxxxx  xx     xx\n");
+                    //principalLibrary.storeObjects();
                     System.out.println(principalLibrary.showObjects());
                     System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
                     subMenu();
                 } else if (selection == 2) {
                     System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
-                    System.out.println("¡Gracias!, vuelva pronto.");
+                    System.out.println("Cerrando programa...");
                     System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
                     flag = 2;
+                    System.exit(0);
                 }
             } while (flag != 2);
         }catch (InputMismatchException exception) {
@@ -86,10 +97,9 @@ public class ClasePrincipal {
                     System.out.println("    3. Filtrar por Genero.");
                     System.out.println("    4. Ordenar por Fecha.");
                     System.out.println("    5. Ordenar por Duración.");
-                    System.out.println("    6. Salir.");
+                    System.out.println("    6. Volver al menú principal.");
                     System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
                     selection = in.nextInt();
-
                     if (selection >= 1 && selection <= 6) {
                         flag = 1;
                     } else {
@@ -128,9 +138,7 @@ public class ClasePrincipal {
                     System.out.println(principalLibrary.showObjects());
 
                 }else if(selection == 6){
-                    System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
-                    System.out.println("¡Gracias!, vuelva pronto.");
-                    System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+                    mainMenu();
                     flag = 2;
                 }
             } while (flag != 2);
@@ -160,7 +168,7 @@ public class ClasePrincipal {
                     System.out.println("    2. Filtrar por Genero.");
                     System.out.println("    3. Ordenar por Fecha.");
                     System.out.println("    4. Ordenar por Duración.");
-                    System.out.println("    5. Salir.");
+                    System.out.println("    5. Devolverse al menu anterior.");
                     System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
                     selection = in.nextInt();
 
@@ -192,9 +200,20 @@ public class ClasePrincipal {
                     System.out.println(playList.getCollectionPlaylist());
                 }else if(selection == 5){
                     System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
-                    System.out.println("¡Gracias!, vuelva pronto.");
+                    System.out.println("/*\n" +
+
+                            "xxxxxxxxxxx   xxxxxxxxxx  xxxxxxxxxxx   xx      xxxxxxxxxx   xxxxxxx   xxxxxxxxxx xxxxxxxx    xxxxxxxx  xxxxxxxxx\n" +
+                            "xx        xx      xx      xx        xx  xx          xx     xxx     xxx     xx     x          xx         xx     xx\n" +
+                            "xx        xx      xx      xx          x xx          xx    x           xx   xx     x        x            xx     xx\n" +
+                            "xx       xx       xx      xx         xx xx          xx    x            x   xx     x        x            xx     xx\n" +
+                            "xxxxxxxxxxx       xx      xxxxxxxxxxxx  xx          xx    x            x   xx     xxxxxxxx x            xxxxxxxxx\n" +
+                            "xx        xx      xx      xx         xx xx          xx    xx           x   xx     x        x            xx     xx\n" +
+                            "xx        xx      xx      xx        xxx xx          xx      xx       xx    xx     x         xx          xx     xx\n" +
+                            "xxxxxxxxxxx   xxxxxxxxxx  xxxxxxxxxxxx  xxxxxxxx xxxxxxxxxx  xxxxxxxxx     xx     xxxxxxxxx  xxxxxxxxx  xx     xx\n");
+                    System.out.println(principalLibrary.showObjects());
                     System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
                     flag = 2;
+                    subMenu();
                 }
             } while (flag != 2);
         }catch (InputMismatchException exception) {
